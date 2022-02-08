d50ce44473e447639adcd0d344a15d1f7a1a673f
Dag Framstad <15799259+dagframstad@users.noreply.github.com>
2022-02-08 09:56:54 +0100

    json parsing

--- dispatch/dispatch.sh
+++ dispatch/dispatch.sh
@@ -11,7 +11,7 @@ echo curl -X POST "https://api.github.com/repos/$OWNER/$REPO/actions/workflows/$
     -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer XXX" \
-    --data "{\"ref\":\"${REF}\",\"inputs\":${INPUT}}"
+    --data "{\"ref\":\"${REF}\",\"inputs\":${INPUTS}}"
 
 curl -X POST "https://api.github.com/repos/$OWNER/$REPO/actions/workflows/$ACTION/dispatches" \
     -H "Accept: application/vnd.github.v3+json" \
