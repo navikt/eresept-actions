3cc4b42eb516fd19810edc1a27eb8a33af3ce8c4
Dag Framstad <15799259+dagframstad@users.noreply.github.com>
2022-02-08 09:25:44 +0100

    debug

--- dispatch/dispatch.sh
+++ dispatch/dispatch.sh
@@ -7,6 +7,12 @@ TOKEN=$4
 REF=$5
 INPUTS=$6
 
+echo curl -X POST "https://api.github.com/repos/$OWNER/$REPO/actions/workflows/$ACTION/dispatches" \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer XXX" \
+    --data "{\"ref\":\"${REF}\",\"inputs\":${INPUT}}"
+
 curl -X POST "https://api.github.com/repos/$OWNER/$REPO/actions/workflows/$ACTION/dispatches" \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: application/json" \
