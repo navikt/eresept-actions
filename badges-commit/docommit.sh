b8a0d4a9802387b745c9338e4dbed6ae87705d55
Dag Framstad <15799259+dagframstad@users.noreply.github.com>
2022-02-16 09:57:14 +0100

    prøver commit igjen om det feiler. 5 forsøk med 5 sekunder sleep mellom.

---
+++ badges-commit/docommit.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+# does the commit
+function doOneCommit() {
+  git config user.name "GitHub Actions Bot (${{ github.actor }})"
+  git config user.email "actions@github.com"
+  git pull
+  git add *.svg
+  git commit -m "Generated badges" *.svg
+  git push
+}
+
+function commit() {
+  local cnt=5
+  while [ $cnt -gt 0 ];
+  do
+    doOneCommit
+    if [ $? -eq 0 ];
+    then
+      cnt=0
+    else
+      # if commit fails we sleep and try again
+      sleep 5
+      cnt=$((cnt - 1))
+    fi
+  done
+}
+
+commit
