7004a588a486e5bb758b31c6ad13c28368c599e1
Dag Framstad <15799259+dagframstad@users.noreply.github.com>
2022-04-06 21:35:45 +0200

    feil param og manglende slash

--- badges-jacoco/mkbadges.sh
+++ badges-jacoco/mkbadges.sh
@@ -3,7 +3,7 @@
 F=$1
 DIR=$2
 SCRIPT=$3/runcurl.sh
-LOGO="$($3../src/getlogo.sh code)"
+LOGO="$($DIR/../src/getlogo.sh code)"
 
 awk -f $DIR/coverage.awk -v logo="$LOGO" $F > $SCRIPT
 awk -f $DIR/lines-and-methods.awk -v logo="$LOGO" $F >> $SCRIPT
