139472ec55bb033401ca040b044cd997c5b23c42
Dag Framstad <15799259+dagframstad@users.noreply.github.com>
2025-03-28 09:37:02 +0100

    overstyre version dersom vi angir noen i popup

--- src/next-version.sh
+++ src/next-version.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/bash
+appver=$1
 dt=$(date '+%Y%m%d-%H%M%S')
 branch=$(git rev-parse --abbrev-ref HEAD)
 stag=$dt
 if [ "$branch" != "main" ]
 then
   stag=$dt-$branch
+fi
+if [ -n "$appver" ]
+then
+  stag=$appver
 fi
 echo $stag
