6dd5e7cd6ff90757e9bbab224fb9034fd1de5fca
Dag Framstad <15799259+dagframstad@users.noreply.github.com>
2026-06-24 09:42:23 +0200

    la til git config

--- src/branch-exists.sh
+++ src/branch-exists.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-
+git config user.email "$ACTOR@users.noreply.github.com" && git config user.name "Github Actions Bot ($ACTOR)"
 git ls-remote --exit-code --heads origin badges
 echo -n $?
 
