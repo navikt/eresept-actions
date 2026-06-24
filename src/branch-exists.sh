edf73c51c047fd02b92c66e2db24c51b724c0cf9
Dag Framstad <15799259+dagframstad@users.noreply.github.com>
2026-06-24 12:22:32 +0200

    echo text

--- src/branch-exists.sh
+++ src/branch-exists.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 git config --global url."https://x-access-token:${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
 git ls-remote --exit-code --heads origin badges
-echo -n $?
+echo -n "$?"
 
