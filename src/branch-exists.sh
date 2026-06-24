4b8482e20fe715f868b833bc1116a2c318538af7
Dag Framstad <15799259+dagframstad@users.noreply.github.com>
2026-06-24 13:01:13 +0200

    stderr and stdout to /dev/null

--- src/branch-exists.sh
+++ src/branch-exists.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 git config --global url."https://x-access-token:${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
-git ls-remote --exit-code --heads origin badges
+git ls-remote --exit-code --heads origin badges > /dev/null 2>&1
 echo -n "$?"
 
