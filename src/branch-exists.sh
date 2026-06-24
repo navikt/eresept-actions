d149ca007631660b5f102d8d9dbb155587979f35
Dag Framstad <15799259+dagframstad@users.noreply.github.com>
2026-06-24 10:15:39 +0200

    git config url.

--- src/branch-exists.sh
+++ src/branch-exists.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-git config user.email "$ACTOR@users.noreply.github.com" && git config user.name "Github Actions Bot ($ACTOR)"
+git config --global url."https://x-access-token:${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
 git ls-remote --exit-code --heads origin badges
 echo -n $?
 
